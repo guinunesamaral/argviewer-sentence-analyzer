a4438ea4d61cd89eb790202be067b2f6d0ddd3cd
Guilherme Nunes Amaral dos Santos <guilherme.santos@vikservices.com.br>
2022-11-15 22:37:35 -0300

    Update

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from flask import Flask
 from flask import jsonify
 from sentence_similarity import compare
 from sentence_profanity import check_profanity
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/api/similarity', methods=["POST"])
